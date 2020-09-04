58b4d14b63a6d791a50aa963bb3dca6fab303592
coms_zhou <610209027@qq.com>
2020-09-05 01:43:48 +0800

    Scala简单编程

---
+++ src/main/scala/Comment.scala
@@ -0,0 +1,24 @@
+/**
+ * @Author: CMOS
+ * @Date: 2020/9/5 1:42 上午
+ */
+/**
+ * @Author: cmos
+ * @Date: 2020/9/5 01:42
+ * @ClassName Comment
+ */
+object Comment {
+  def main(args: Array[String]): Unit = {
+    print("hi")
+  }
+
+  /**
+   *
+   * @param n1
+   * @param n2
+   * @return 和
+   */
+  def sum(n1: Int, n2: Int): Int = {
+    return n1 + n2
+  }
+}
--- src/main/scala/helloscala.scala
+++ src/main/scala/helloscala.scala
@@ -1,5 +1,29 @@
 object helloscala {
   def main(args: Array[String]): Unit = {
     print("hi,scala~")
+//scala语言输出的三种方式
+    var num1 : Int = 1
+    var num2 : Int =2
+    print(num1+num2)
+    var name:String = "tom"
+    var age: Int = 10
+    var sal:Float = 10.67f
+    var height:Double =120.15
+
+    // printf 格式化输出
+    // %d 十进制数字
+    // %s 字符串
+    // %c 字符
+    // %e 指数浮点数
+    // %f 浮点数
+    printf("名字%s 年龄%d 薪水%f 身高%f",name,age,sal,height)
+    printf("名字%s 年龄%d 薪水%.2f 身高%.3f",name,age,sal,height)
+    println(s"名字$name 年龄$age 薪水$sal")
+    println(s"名字$name \t年龄${age+10} \t薪水${sal*9}")
+
+
+
+
   }
+
 }
