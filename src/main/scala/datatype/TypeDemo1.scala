dd6de5833c3e5648984c7375a35728d4e41882ab
coms_zhou <610209027@qq.com>
2020-09-06 23:55:32 +0800

    隐式转换

--- src/main/scala/datatype/TypeDemo1.scala
+++ src/main/scala/datatype/TypeDemo1.scala
@@ -14,6 +14,12 @@ object TypeDemo1 {
     var num1 : Int = 10
     //因为Int是一个类 所以它的实例可以使用很多方法
     println(num1.toDouble + num1.toString)
+
+    var num4 =9.3
+    var num5 = 9.8f
+    num4 =num5
+    num5 = num4.toFloat //4精度高
+
   }
 
   //scala中如果一个方法没有形参，则可以省略()
@@ -22,5 +28,10 @@ object TypeDemo1 {
   }
   def sayhello = {
     println("hello")
+  }
+
+  //在开发中 比如有一个方法 就会异常中断 这时就可以返回nothing  即当我们nothing做返回值 就是明确的说明该方法没有正常返回值
+  def sayproblem : Nothing = {
+    throw new Exception("抛出异常")
   }
 }
