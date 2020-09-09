ef2296d5665fa0cb7cfbe982d9019a0cf293460c
coms_zhou <610209027@qq.com>
2020-09-09 23:10:36 +0800

    方法函数介绍

--- src/main/scala/meth_func/meth_funcDemo1.scala
+++ src/main/scala/meth_func/meth_funcDemo1.scala
@@ -1,9 +1,30 @@
 package meth_func
 
 /**
-  * @author 光子郎
-  * @date 2020/9/9 22:45
-  */
+ * @Author: cmos
+ * @Date: 2020/9/5 01:42
+ * @ClassName Comment
+ */
 object meth_funcDemo1 {
+  def main(args: Array[String]): Unit = {
+    val dog = new Dog
 
+    //方法转函数
+    val f1 = dog.sum _
+    println(f1) //<function2> 其中2代表有两个形参
+    println(f1(50,40)) // 90
+
+    //直接写一个函数
+    val f2 = (n1:Int,n2:Int) => {
+      n1+n2
+    }
+    println(f2(50,40)) //90
+  }
+}
+
+class Dog{
+  //方法
+  def sum(n1:Int,n2:Int):Int = {
+    n1 + n2
+  }
 }
